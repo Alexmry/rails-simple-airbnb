41adacbf165efbb32c523ef97c02eba660eda7e8
Alexandre Morey <alexander006_9@hotmail.com>
2021-05-28 15:45:14 +0100

    Rails new with frontend and form gems

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'pages#home'
+  resources :flats, except: :index do
+    collection do
+      get :top
+    end
+  end
 end
