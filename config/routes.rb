60e52793704886372963e06ce923468cb9317211
Alexandre Morey <alexander006_9@hotmail.com>
2021-05-28 17:43:49 +0100

    show page design

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'pages#home'
-  resources :flats, except: :index do
-    collection do
-      get :top
-    end
-  end
+  root to: 'flats#index'
+  resources :flats
 end
