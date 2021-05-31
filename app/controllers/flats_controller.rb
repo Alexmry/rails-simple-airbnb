efc497eb04e4d4f659a848256821b56e4ca41ef3
Alexandre Morey <alexander006_9@hotmail.com>
2021-05-31 15:30:25 +0100

    show method

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -2,4 +2,14 @@ class FlatsController < ApplicationController
   def index
     @flats = Flat.all
   end
+
+  def show
+    @flat = Flat.find(params[:id])
+  end
+
+  private
+
+  def flat_params
+
+  end
 end
